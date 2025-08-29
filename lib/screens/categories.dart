2fa2145861689583d85d9ffcd1f649f042781b78
Yağız Can Dinç <ycandiinc@gmail.com>
2025-08-29 16:49:28 +0300

    first commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pick your category")),
+      //number of columns mainAxis yukarıdan aşağıya cross soldan sağa
+      body: GridView(
+        padding: EdgeInsets.all(24),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2, // gridView item sizing
+          crossAxisSpacing: 20, //spacing between the columns
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          //availableCategories.map((category)=>CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
