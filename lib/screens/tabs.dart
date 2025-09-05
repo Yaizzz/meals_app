81ed0af4a413777f0f21d156a2beeb536ef20378
Yağız Can Dinç <ycandiinc@gmail.com>
2025-09-05 16:50:43 +0300

    173 sonu drawer içerisine filtreleme ekliyoruz

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -45,6 +45,13 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier) {
+    if (identifier == "filters") {
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -61,7 +68,7 @@ class _TabsScreenState extends State<TabsScreen> {
     }
     return Scaffold(
       appBar: AppBar(title: Text(activePageTitle)),
-      drawer: MainDrawer(),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
